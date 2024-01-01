72d8b4b02b89906a13eab13f4e18f877b649d87c
Vladislav <maksic1996@yandex.ru>
2024-01-01 23:29:29 +0300

    change music link for game on neural 8bit music

--- web/static/js/game.js
+++ web/static/js/game.js
@@ -1,5 +1,5 @@
-let randomInt = Math.floor(Math.random() * 150) + 1;  // Generates a random integer between 1 and 150
-let potentialUrl = `https://wladradchenko.ru/static/radio.wladradchenko.ru/lofi/${randomInt}.mp3`;
+// let randomInt = Math.floor(Math.random() * 150) + 1;  // Generates a random integer between 1 and 150
+let potentialUrl = `https://wladradchenko.ru/stream?station=8bit`;
 let defaultUrl = '/static/game/audio/bgG.mp3';
 
 bg = new Audio(potentialUrl);
